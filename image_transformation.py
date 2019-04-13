3118cb30ee50ce6321bae0a5f56e0c560fa6b0a1
shreshtha2503 <shreshtha2503@gmail.com>
2019-04-13 15:58:05 -0400

    Update image_transformation.py

--- image_transformation.py
+++ image_transformation.py
@@ -13,15 +13,15 @@ for i in range(0, width):
         gray_value = (int)(0.07 * blue_component + 0.72 * green_component + 0.21 * red_component)
         image_pixels[i,j] = (gray_value, gray_value, gray_value, 255)
         if(gray_value < 50):
-            new_color = (0, 31, 59, 255)
+            new_color = (184, 232, 201, 255)
         elif(gray_value < 100):
-            new_color = (215, 30, 1, 255)
+            new_color = (188, 255, 248, 255)
         elif(gray_value < 125):
-            new_color = (107, 153, 160, 255)
+            new_color = (240, 255, 234, 255)
         elif(gray_value < 150):
-            new_color = (182, 198, 186, 255)
+            new_color = (255, 238, 167, 255)
         elif(gray_value < 200):
-            new_color = (250, 237, 192, 255)
+            new_color = (193, 189, 186, 255)
         image_pixels[i,j] = (new_color[0], new_color[1], new_color[2], 255)
 
 my_image.show()
